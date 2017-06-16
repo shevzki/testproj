5c5953231c354b203d2f958963b3d955dbe0a455
olya.shevzki <olechka111087@mail.ru>
2017-06-16 15:33:37 +0300

    testproj

---
+++ js/app.js
@@ -0,0 +1,15 @@
+﻿$(function() {
+	alert ('jQuery подключен и отлично работает!');
+
+	$("#header__button").click(function(e) {
+		$(this).toggleClass("hamburger--open");
+
+		var dropDown = $(this).parent().next();
+
+		$('.dropmn').not(dropDown).slideUp('slow');
+
+		dropDown.slideToggle('slow');
+
+		e.preventDefault();
+	});
+});
